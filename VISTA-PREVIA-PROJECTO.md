10630225795bd564ccd5ff1432da54ef4e72fa60
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-09-30 14:39:36 -0300

    asd

---
+++ VISTA-PREVIA-PROJECTO.md
@@ -0,0 +1,3 @@
+# Block-chain-3.0
+[![BLOCK-CHAIN-3-0-DOS.png](https://i.postimg.cc/DyTtz3Xf/BLOCK-CHAIN-3-0-DOS.png)](https://postimg.cc/G9XMMNK6)
+[![Block-chain-3-0.png](https://i.postimg.cc/Vkspj9g3/Block-chain-3-0.png)](https://postimg.cc/yk5LKZwP)
---
+++ src/components/Transactions.jsx
@@ -0,0 +1,18 @@
+import React, {useContext} from "react"
+import { TransactionContext } from "../context/TransactionContext"
+import dummyData from "../utils/dummyData"
+
+
+const Transcations = () => {
+    return (
+        <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+            <div className="flex flex-col md:p-12 py-12 px-4">
+                /*Latest Transactions */
+                /*Connect Your Account To See The Lastest Transactions */
+            </div>
+
+        </div>
+    )
+}
+
+export default Transcations
