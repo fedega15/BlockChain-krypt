6d609c6dbd1c0bddb4b933eab7ae9d02f58cba70
Federico Galassi - Front End Dev <94568896+fedega15@users.noreply.github.com>
2023-09-30 22:33:02 -0300

    asd

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -7,8 +7,7 @@ const Transcations = () => {
     return (
         <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
             <div className="flex flex-col md:p-12 py-12 px-4">
-                /*Latest Transactions */
-                /*Connect Your Account To See The Lastest Transactions */
+    
             </div>
 
         </div>
